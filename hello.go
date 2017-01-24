78953d9814c067d6bbeb1f436a8a5c6b4f1ec736
shuanghun <shuanghun@wacai.com>
2017-01-24 13:44:31 +0800

    add

--- hello.go
+++ hello.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	var b bool = true
+	fmt.Println(b)
+	c := 10
+	fmt.Println(c)
+	var a int = 10
+	fmt.Println(a)
+	var d,e,f = 10, 11, 12
+	fmt.Println(d,e,&f)
 }
